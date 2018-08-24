942877c9b8e221361e02b7980e179677b1d6223e
drcicero <11000518+drcicero@users.noreply.github.com>
2018-08-24 17:57:41 +0200

    call it wochenplaner

--- step2.py
+++ step2.py
@@ -306,7 +306,7 @@ if __name__ == "__main__":
           <html><head>
             <meta charset=utf8>
             <meta name=viewport content="width=device-width, initial-scale=1.0">
-            <title>{{today4}}, {{regulation}}, inoffizielles Wochenplaner TU Darmstadt FB Informatik</title>
+            <title>{{today4}}, {{regulation}}, inoffizieller Wochenplaner TU Darmstadt FB Informatik</title>
             <style>
         {{{css_style}}}
             </style>
@@ -318,7 +318,7 @@ if __name__ == "__main__":
               <p>Zuletzt aktualisiert: {{today2}}</p>
               <p>
 <h2 style=font-size:1em;font-weight:bold
-><span style=color:red>inoffizielles</span> Wochenplaner TU Darmstadt FB Informatik</h2>
+><span style=color:red>inoffizieller</span> Wochenplaner TU Darmstadt FB Informatik</h2>
 <b>Benutzung auf eigene Gefahr!</b>
 Dies ist eine inoffizielle Seite.
 Beachten Sie, das Übungsgruppentermine nicht aufgeführt werden, sondern nur Termine die in Tucan direkt als Veranstaltungstermin gelistet sind. Manchmal finden Termine auch erst ab der zweiten Woche statt.
