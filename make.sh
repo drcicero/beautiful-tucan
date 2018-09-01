dc221b10e4a4b0a7aa16f21b0907ec16e9f3d595
drcicero <11000518+drcicero@users.noreply.github.com>
2018-09-01 16:02:49 +0200

    fix incomplete pjax comment; adapt make.sh

--- make.sh
+++ make.sh
@@ -3,10 +3,10 @@
 export LANG=de_DE.UTF-8
 
 mkdir -p gh-pages
-pip3 install mechanicalsoup bs4 pystache &&
+pip3 install -q mechanicalsoup bs4 pystache &&
 python3 step1.py && # download
 python3 step2.py && # generate
 cp code.js gh-pages/code.js &&
-cp index.html gh-pages/index.html &&
+cp style.css gh-pages/style.css &&
 echo success
 
